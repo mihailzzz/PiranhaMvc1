564ff1f57ea04750a68d2ad996dec9207e286172
Admin-PC <Admin-PC@DESKTOP-74MSG97>
2023-03-10 13:30:57 +0300

    Добавьте файлы проекта.

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace PiranhaMvc1.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage : Page<StandardPage>
+    {
+    }
+}
